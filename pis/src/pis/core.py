55bf1e1c895ceb47b791eb2e54c410eb4f140c68
Javier Ferrer <javier.f.g@um.es>
2026-07-06 18:38:30 +0100

    pis: another thingy

--- pis/src/pis/core.py
+++ pis/src/pis/core.py
@@ -14,7 +14,7 @@ def main() -> None:
     runner.register_tasks('pis.tasks')
     s = asyncio.run(runner.run())
 
-    print('hi')
+    logger.info('hi')
 
     if s.manifest.result not in [Result.PENDING, Result.SUCCESS]:
         logger.error(f'step {s.name} failed')
