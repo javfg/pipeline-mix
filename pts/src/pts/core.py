2ec260ddbdbcea5918334d900159a18ed22525ca
Javier Ferrer <jferrer@ebi.ac.uk>
2026-07-07 16:21:38 +0100

    yet another log

--- pts/src/pts/core.py
+++ pts/src/pts/core.py
@@ -15,6 +15,7 @@ def main() -> None:
     s = asyncio.run(runner.run())
 
     logger.info('hi')
+    logger.info('hi!')
 
     if s.manifest.result not in [Result.PENDING, Result.SUCCESS]:
         logger.error(f'step {s.name} failed')
