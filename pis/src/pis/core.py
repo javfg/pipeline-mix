a8aead0aecbdfa3ce98077d1690713c4b6f5b48c
Javier Ferrer <jferrer@ebi.ac.uk>
2026-07-07 14:25:03 +0100

    say hi

--- pis/src/pis/core.py
+++ pis/src/pis/core.py
@@ -14,6 +14,8 @@ def main() -> None:
     runner.register_tasks('pis.tasks')
     s = asyncio.run(runner.run())
 
+    logger.info('hi')
+
     if s.manifest.result not in [Result.PENDING, Result.SUCCESS]:
         logger.error(f'step {s.name} failed')
         raise SystemExit(1)
